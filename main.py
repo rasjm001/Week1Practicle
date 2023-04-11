2299dfdb0a6f43c8af6c15807fa604ffe61cf988
James Rasche <rasjm001@mymail.unisa.edu.au>
2023-04-11 21:46:45 +0930

    Starting Template for Practical

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # Week One Practical
 
-#Question one
+# Question one
+def distance(velocity, time):
+    distance_travelled = velocity * time
+    return distance_travelled
 
+
+def kinetic_energy(mass, velocity):
+    kenegry = (0.5 * mass) * velocity * velocity
+    return kenegry
+
+
+# ask user to input mass in kilograms
+user_input_mass = int(input('please enter mass in kilograms: '))
+
+# ask user to input velocity in meters per second
+user_input_velocity = int(input("please enter velocity in meters per second: "))
+
+print(kinetic_energy(user_input_mass, user_input_velocity))
